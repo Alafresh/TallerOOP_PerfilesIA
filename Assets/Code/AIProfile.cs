fc1c1043d7256c87002d4347f3e11e19b48654d0
Alafresh <51685896+Alafresh@users.noreply.github.com>
2023-09-02 18:50:43 -0500

    update scene and IAsProfiles

--- Assets/Code/AIProfile.cs
+++ Assets/Code/AIProfile.cs
@@ -4,85 +4,12 @@ using System.Collections.Generic;
 using System.Net.NetworkInformation;
 using UnityEngine;
 
-public abstract class AIProfile
+public abstract class AIProfile : MonoBehaviour 
 {
     protected abstract float viewDistance { get; }
     protected abstract float waitForTurn { get; }
     protected abstract List<Vector3> waypoints { get; }
-    CharacterController target;
     public abstract void ExecuteProfile();
     public virtual void OnPlayerDetected() => Debug.Log("Player found");
     public abstract void DetectPlayer();
-}
-
-public class Guardia : AIProfile
-{
-    protected override float viewDistance { get => 10f; }
-
-protected override float waitForTurn  { get => 5f; }
-
-    protected override List<Vector3> waypoints => throw new System.NotImplementedException();
-
-    public override void DetectPlayer()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public override void ExecuteProfile()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public override void OnPlayerDetected()
-    {
-        base.OnPlayerDetected();
-    }
-}
-
-public class PatrullaCiclica: AIProfile
-{
-    protected override float viewDistance => throw new System.NotImplementedException();
-
-    protected override float waitForTurn => throw new System.NotImplementedException();
-
-    protected override List<Vector3> waypoints => throw new System.NotImplementedException();
-
-    public override void DetectPlayer()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public override void ExecuteProfile()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public override void OnPlayerDetected()
-    {
-        base.OnPlayerDetected();
-    }
-}
-
-public class PatrullaPingPong : AIProfile
-{
-    protected override float viewDistance => throw new System.NotImplementedException();
-
-    protected override float waitForTurn => throw new System.NotImplementedException();
-
-    protected override List<Vector3> waypoints => throw new System.NotImplementedException();
-
-    public override void DetectPlayer()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public override void ExecuteProfile()
-    {
-        throw new System.NotImplementedException();
-    }
-
-    public override void OnPlayerDetected()
-    {
-        base.OnPlayerDetected();
-    }
 }
--- Assets/Code/CharacterController.cs
+++ Assets/Code/CharacterController.cs
@@ -11,7 +11,7 @@ public class CharacterController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        anim = this.GetComponent<Animator>();
+        //anim = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -24,26 +24,24 @@ public class CharacterController : MonoBehaviour
         translation *= Time.deltaTime;
         rotation *= Time.deltaTime;
 
-        if (Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.DownArrow) ||
-            Input.GetKeyUp(KeyCode.LeftArrow) || Input.GetKeyUp(KeyCode.RightArrow))
-            anim.SetBool("isWalking", false);
+       
         if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.DownArrow))
         {
-            anim.SetBool("isWalking", true);
+            //anim.SetBool("isWalking", true);
             transform.Translate(0, 0, translation);
         }
         if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
         {
-            anim.SetBool("isWalking", true);
+            //anim.SetBool("isWalking", true);
             transform.Rotate(0, rotation, 0);
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
-            anim.SetTrigger("isJumping");
-        else if (Input.GetKeyDown(KeyCode.P))
-            anim.SetTrigger("isPunching");
-        else if (Input.GetKeyDown(KeyCode.K))
-            anim.SetTrigger("isKicking");
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //    anim.SetTrigger("isJumping");
+        //else if (Input.GetKeyDown(KeyCode.P))
+        //    anim.SetTrigger("isPunching");
+        //else if (Input.GetKeyDown(KeyCode.K))
+        //    anim.SetTrigger("isKicking");
 
     }
 }
